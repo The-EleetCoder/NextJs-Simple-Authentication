8ae1226f74395e1c38a78a57d94841405a1002d4
The-EleetCoder <jaijain1803@gmail.com>
2024-07-13 17:02:10 +0530

    verifyEmail route created

---
+++ src/app/api/users/verifyEmail/route.ts
@@ -0,0 +1,34 @@
+import { connect } from "@/dbConfig/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+
+connect();
+
+export async function POST(req: NextRequest) {
+  try {
+    const reqBody = await req.json();
+    const { token } = reqBody;
+
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Invalid token" }, { status: 400 });
+    }
+
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+
+    await user.save();
+
+    return NextResponse.json(
+      { message: "Email verified successfully", success: true },
+      { status: 200 }
+    );
+  } catch (e: any) {
+    return NextResponse.json({ error: e.message }, { status: 500 });
+  }
+}
