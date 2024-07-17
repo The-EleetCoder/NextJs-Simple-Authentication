ad5315e4476d0844af99820d6f033e8b474dfadb
The-EleetCoder <jaijain1803@gmail.com>
2024-07-17 11:36:27 +0530

    verify email page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,7 +5,7 @@ import { toast } from 'react-hot-toast'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 
-const Signup = () => {
+const SignupPage = () => {
   const router = useRouter();
   const [user, setUser] = useState({
     email: "",
@@ -15,13 +15,13 @@ const Signup = () => {
   const [buttonDisabled, setButtonDisabled] = useState(true);
   const [loading, setLoading] = useState(false);
 
-useEffect(()=>{
-  if (user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
-    setButtonDisabled(false);
-  } else {
-    setButtonDisabled(true);
-  }
-},[user])
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user])
 
   const onSignup = async () => {
     try {
@@ -36,7 +36,7 @@ useEffect(()=>{
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>{loading ? "Processing" : "Signup"}</h1>
+      <h1 className='mb-5 text-[2rem] font-[600]'>{loading ? "Processing" : "Signup"}</h1>
       <hr />
       <label htmlFor="username">username</label>
       <input
@@ -66,11 +66,12 @@ useEffect(()=>{
         placeholder="password"
       />
       <button
+        disabled={buttonDisabled}
         onClick={onSignup}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">{buttonDisabled ? "No signup" : "Signup"}</button>
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Signup</button>
       <Link href="/login">Visit login page</Link>
     </div>
   )
 }
 
-export default Signup
+export default SignupPage
